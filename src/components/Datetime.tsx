72353048159d1c2c777d5197ef4682774831357b
Charles <8595764@qq.com>
2024-01-24 00:44:32 +0800

    回到顶部

--- src/components/Datetime.tsx
+++ src/components/Datetime.tsx
@@ -30,7 +30,7 @@ export default function Datetime({
       </svg>
       {modDatetime ? (
         <span className={`italic ${size === "sm" ? "text-sm" : "text-base"}`}>
-          Updated:
+          最近更新:
         </span>
       ) : (
         <span className="sr-only">Published:</span>
