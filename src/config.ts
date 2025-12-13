da67dc46ddc2403f14866f74e69211504c6acb27
Charles <8595764@qq.com>
2025-12-13 16:37:33 +0800

    Increase post limits in config

--- src/config.ts
+++ src/config.ts
@@ -6,8 +6,8 @@ export const SITE = {
   title: "Charles",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
-  postPerIndex: 4,
-  postPerPage: 4,
+  postPerIndex: 8,
+  postPerPage: 10,
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
   showArchives: true,
   showBackButton: true, // show back button in post detail
